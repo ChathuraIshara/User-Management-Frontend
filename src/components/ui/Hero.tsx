6290a6bb684b65a3f6a030fd155db288ad9f02e4
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-08-03 15:29:46 +0530

    Enhance Hero component styling with border, shadow, and padding

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -4,7 +4,7 @@ import UserTable from "./UserTable";
 
 export default function Hero(){
     return (
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 border border-gray-200 rounded-xl shadow-sm bg-white p-6 mt-5">
            <OverviewDescription />
            <SearchAndFilter />
            <UserTable/>
